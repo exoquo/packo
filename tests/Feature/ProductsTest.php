61e8ee4f980c6564501d0509eed6d7043ad34de5
Jonas Steinkamp <jonas@steinka.mp>
2020-05-02 09:35:14 +0200

    Add first Test

---
+++ tests/Feature/ProductsTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProductsTest extends TestCase
+{
+    /** @test */
+    public function a_user_can_create_a_product()
+    {
+        return true;
+    }
+}
