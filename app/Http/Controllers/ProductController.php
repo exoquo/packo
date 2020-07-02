a3152f8f3de0225fa72d83f270964c34d0346d88
Jonas <jonas@exoquo.com>
2020-07-03 00:34:37 +0200

    add a Product

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Product::all()->toJson();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //validate
+        Product::create(request(['name', 'sku']));
+        //redirect
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Product $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return $product->toJson();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Product $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Product $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -2,15 +2,40 @@
 
 namespace Tests\Feature;
 
+use App\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ProductsTest extends TestCase
 {
+    use WithFaker, RefreshDatabase;
+
+
     /** @test */
     public function a_user_can_create_a_product()
     {
-        return true;
+        $this->withoutExceptionHandling();
+
+        $attributes = [
+            'name' => 'Test'
+        ];
+
+        $this->post('/api/products', $attributes);
+
+        $this->assertDatabaseHas('products', $attributes);
+
+        $this->get('/api/products')->assertSee($attributes['name']);
+    }
+
+    /** @test */
+    public function a_user_can_view_a_product()
+    {
+        $this->withoutExceptionHandling();
+
+        $product = factory('App\Product')->create();
+
+        $this->get('/api/products/' . $product->id)
+            ->assertSee($product['name']);
     }
 }
