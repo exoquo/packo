ab633d57a7d2f913bc4b27e8c4ffeb35e5b603fd
Jonas <jonas@exoquo.com>
2020-07-09 21:55:04 +0200

    Add a Warehouse

---
+++ database/factories/WarehouseFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Warehouse;
+use Faker\Generator as Faker;
+
+$factory->define(
+    Warehouse::class,
+    function (Faker $faker) {
+        return [
+            'name' => $faker->name,
+        ];
+    }
+);
